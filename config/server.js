96cebb6e75f7f6e7c9963520793c969aa33eae2e
Christy Mathew <christymathew9742@gmail.com>
2022-07-01 16:27:01 +0530

    added API key

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS',['Ps0UXPMfteby51iVeB2hcw==','3vhpEq0KHOvAsAfINHjG1Q==','5lqSIHihWellr71VY2q6Ew==','K5vkS9Pli7Ld/JaoLkngnQ==']),
   },
 });
